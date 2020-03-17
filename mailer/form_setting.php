1d25a34f05508f4e3c6170b4aae159cf3aaf1271
Adebayo John <Kentjohn165@gmail.com>
2020-03-17 19:22:57 +0100

    Add files via upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from thejohnadebayo.orn.ng 'Website'"; 
+	$to = 'kentjohn165@gmail.com'; 
+	$from = 'admin@you-site-name.com'; 
+	$fromName = '#'; 
+	$charset = 'UTF-8';
+?>
